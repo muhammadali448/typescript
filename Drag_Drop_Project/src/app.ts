4b652a0c997422ad5745b52574d03bdd4e899fc0
cq-m-ali <muhammad.ali@cqtechnologies.com>
2020-08-11 19:58:46 +0500

    autobind and userInputs

--- Drag_Drop_Project/src/app.ts
+++ Drag_Drop_Project/src/app.ts
@@ -1,4 +1,14 @@
-
+function AutoBind(_target: any, _methodName: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const bindFunc = originalMethod.bind(this);
+            return bindFunc
+        }
+    }
+    return adjDescriptor;
+}
 class ProjectInput {
     templateEl: HTMLTemplateElement;
     divEl: HTMLDivElement;
@@ -21,11 +31,36 @@ class ProjectInput {
     private attach() {
         this.divEl.insertAdjacentElement("afterbegin", this.formEl);
     }
+    private takeUserInputs(): [string, string, number] | void {
+        const title = this.titleInputEl.value;
+        const description = this.descriptionInputEl.value;
+        const people = this.peopleInputEl.value;
+        if (title.trim().length === 0 ||
+            description.trim().length === 0 ||
+            people.trim().length === 0) {
+            alert("Please fill all fields");
+        }
+        else {
+            return [title, description, +people];
+        }
+    }
+    private clearAllFields() {
+        this.titleInputEl.value = "";
+        this.descriptionInputEl.value = "";
+        this.peopleInputEl.value = "";
+    }
+    @AutoBind
     private handleSubmitForm(e: Event) {
         e.preventDefault();
+        const userInputs = this.takeUserInputs();
+        if (Array.isArray(userInputs)) {
+            const [title, description, people] = userInputs;
+            console.log(title, description, people);
+            this.clearAllFields();
+        }
     }
     private configure() {
-        this.formEl.addEventListener("submit", this.handleSubmitForm.bind(this));
+        this.formEl.addEventListener("submit", this.handleSubmitForm);
     }
 }
 
