d117165ea64689a3a8239805854a62c1cd192f11
cq-m-ali <muhammad.ali@cqtechnologies.com>
2020-08-16 12:57:15 +0500

    classes

--- practiceTypescript/src/app.ts
+++ practiceTypescript/src/app.ts
@@ -1,46 +1,147 @@
-// Interfaces examples
+// Constructors example
+class Training {
+  constructor(
+    private category: string,
+    private title: string,
+    private noOfDays: number
+  ) {}
+  buy() {
+    console.log(
+      "Buying this " +
+        this.noOfDays +
+        " day(s) " +
+        this.title +
+        " course from category " +
+        this.category
+    );
+  }
+}
+// Constructor parameters are mapped to member variables.
+// If we prefix a constructor parameter with an access modifier (ie. private),
+// it will automatically be mapped for us.
+// We can refer to these constructor parameters as if they were declared
+// as properties on the class, for example this.title, can be used anywhere within the Training class
+// to obtain the Training title on that instance
+class BuyTraining {
+  constructor(private trainings: Training[]) {}
+  buy() {
+    var training = this.chooseTraining();
+    training.buy();
+  }
+  private chooseTraining() {
+    // Decision can come for example from the form in the browser, another webservice, db, etc
+    var whichTraining = 2;
+    return this.trainings[whichTraining];
+  }
+}
+var trainings = [
+  new Training("Drupal", "Drupal 8 for Developers", 2),
+  new Training("Angular", "Angular 2 Fundamentals", 3),
+  new Training("Nodejs", "Developing web applications with the MERN stack", 5),
+  new Training("SQL", "T-SQL basics", 2),
+  new Training("Management", "BPMN for code architects", 3),
+];
+var choice = new BuyTraining(trainings);
+choice.buy();
 
-interface CourseVenue {
-    // Properties
-    city: string;
-    country: string;
+// Create structure about songs and make jukebox,
+// which will randomly get the song from the list of songs.
+
+class Song {
+  constructor(
+    private title: string,
+    private artist: string,
+    private year: number
+  ) {}
+  printInfo() {
+    console.log(
+      `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}`
+    );
+  }
 }
 
-interface CourseParticipant {
-    // Properties
-    name: string;
+class JukeBox {
+  constructor(private songs: Song[]) {}
+  playSong() {
+    const song = this.chooseRandomSong();
+    song.printInfo();
+  }
+  private chooseRandomSong(): Song {
+    return this.songs[Math.floor(Math.random() * this.songs.length)];
+  }
 }
 
-interface CourseEvent {
-    // Constructor
-    new(): CourseEvent;
-    // Properties
-    currentLocation: CourseVenue;
-    // Methods
-    // bookVenue(venue: CourseVenue);
-    // addDelegate(delegate: CourseParticipant);
-    // removeDelegate(delegate: CourseParticipant);
+const jukebox = new JukeBox([
+  new Song("Forgive Me", "Chloe x Halle", 2020),
+  new Song("Exile", "Taylor Swift and Bon Iver", 2020),
+  new Song("Water", "Kehlani", 2020),
+  new Song("Ferris Wheel", "Sylvan Esso", 2020),
+  new Song("On My Own", "Shamir", 2020),
+]);
+
+jukebox.playSong();
+
+// Properties and methods
+class CartWithTrainings {
+  private trainings: Training[] = [];
+
+  static maxTraining: number = 10;
+
+  constructor(public cartId: string) {}
+
+  addTraining(training: Training) {
+    if (this.trainings.length >= CartWithTrainings.maxTraining) {
+      throw new Error("To many courses in your Cart.");
+    }
+    this.trainings.push(training);
+  }
+
+  public get getTrainings(): Training[] {
+    return this.trainings;
+  }
 }
 
-// Create a set of interfaces to describe a vehicle, passengers, location, and destination.
-// Declare properties and methods using type annotations.
-// Declare constructors using the ''new'' keyword.
+// Creating a new instance
+var coursesCart = new CartWithTrainings("Cart1");
 
-interface vehicle {
-    carType: string;
-    wheels: number;
-}
-interface passengers {
-    isMale: boolean;
-    noOfSeats: number;
-}
-interface location {
-    address: string
-}
+// Accessing a public instance property
+var nameCart = coursesCart.cartId;
 
-interface Destination {
-    vehicle: vehicle;
-    passengers: passengers;
-    location: location;
-    new(): Destination;
+// Calling a public instance method
+coursesCart.addTraining(new Training("GIT", "Git for Users", 1));
+
+// Accessing a public static property
+var maxTrainings = CartWithTrainings.maxTraining;
+console.log(coursesCart.getTrainings);
+// Make a Playlist of songs (reuse code from exercise 17)
+// - declare class for playlist
+// -- use private and static properties
+// - prepare 'addSong' method
+// - create new instance, access and call:
+// -- playlist, its public instance property, public instance method,
+// public static property
+
+class Playlist {
+  private songs: Song[] = [];
+  static maximumSongs: number = 5;
+  constructor(public playListId: string) {}
+  addSong(song: Song) {
+    if (this.songs.length >= Playlist.maximumSongs) {
+      throw new Error("To many songs in your Playlist.");
+    }
+    this.songs.push(song);
+  }
+
+  public get getSongs(): Song[] {
+    return this.songs;
+  }
 }
+// Creating a new instance
+const playlist = new Playlist("Plalist-1");
+// Accessing a public instance property
+var nameCart = playlist.playListId;
+// Calling a public instance method
+playlist.addSong(new Song("In the end", "Linkin Park", 1));
+// Accessing a public static property
+var maxSongs = Playlist.maximumSongs;
+console.log(playlist.getSongs);
---
+++ practiceTypescript/src/interfaces_2.ts
@@ -0,0 +1,58 @@
+// Interfaces examples
+
+interface CourseVenue {
+    // Properties
+    city: string;
+    country: string;
+}
+
+interface CourseParticipant {
+    // Properties
+    name: string;
+}
+
+interface CourseEvent {
+    // Constructor
+    new(): CourseEvent;
+    // Properties
+    currentLocation: CourseVenue;
+    // Methods
+    // bookVenue(venue: CourseVenue);
+    // addDelegate(delegate: CourseParticipant);
+    // removeDelegate(delegate: CourseParticipant);
+}
+
+// Create a set of interfaces to describe a vehicle, passengers, location, and destination.
+// Declare properties and methods using type annotations.
+// Declare constructors using the ''new'' keyword.
+
+interface vehicle {
+    carType: string;
+    wheels: number;
+}
+interface passengers {
+    isMale: boolean;
+    noOfSeats: number;
+}
+interface location {
+    address: string
+}
+
+interface Destination {
+    vehicle: vehicle;
+    passengers: passengers;
+    location: location;
+    new(): Destination;
+}
+
+// Extending the NodeList interface example
+interface NodeList {
+    // Function arrow shortcut (event case)
+    onclick: (event: MouseEvent) => any;
+}
+
+var nodeList = document.getElementsByTagName('div');
+
+// nodeList.onclick = function (_: MouseEvent) {
+//     alert('Clicked');
+// };
